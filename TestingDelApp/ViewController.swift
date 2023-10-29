2345623d4eec535c6164aa5e3a2edcc3133b0ece
lebrozaurjagger <lebrozaur@icloud.com>
2023-10-29 15:41:02 +0300

    Initial Commit

---
+++ TestingDelApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestingDelApp
+//
+//  Created by Phillip Jagger on 29.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
